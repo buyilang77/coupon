b2850737ef56d2afd3a612e5369ae32719d3d110
Ethan Ke <zhennankee@gmail.com>
2020-12-17 10:24:07 +0800

    Update coupon resources

--- app/Http/Resources/Merchant/CouponItemResource.php
+++ app/Http/Resources/Merchant/CouponItemResource.php
@@ -15,10 +15,14 @@ class CouponItemResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'       => $this->id,
-            'code'     => $this->code,
-            'status'   => $this->status,
-            'password' => $this->password,
+            'id'                     => $this->resource->id,
+            'qr_code_link'           => config('domain.h5') . '/#/coupons/' . $this->resource->coupon->id,
+            'code'                   => $this->resource->code,
+            'password'               => $this->resource->password,
+            'open_status'            => $this->resource->open_status,
+            'open_status_text'       => $this->resource->open_status_text,
+            'redemption_status'      => $this->resource->redemption_status,
+            'redemption_status_text' => $this->resource->redemption_status_text,
         ];
     }
 }
