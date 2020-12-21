3100c8b043cb35fa578eff37820c622cd8314773
Ethan Ke <zhennankee@gmail.com>
2020-12-21 18:22:33 +0800

    Add return field ID

--- app/Http/Resources/Frontend/CouponResource.php
+++ app/Http/Resources/Frontend/CouponResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources\Frontend;
 
-use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -11,14 +10,15 @@ class CouponResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'title' => $this->resource->title,
-            'products' => Product::whereIn('id', $this->resource->products)->get(['id', 'name', 'price', 'carousel', 'description']),
+            'id'              => $this->resource->id,
+            'title'           => $this->resource->title,
+            'products'        => Product::whereIn('id', $this->resource->products)->get(['id', 'name', 'price', 'carousel', 'description']),
             'total_shipments' => $this->resource->order->count(),
         ];
     }
