d6bf49bf49f4bda87d42c8944dcb176e015a05ea
Ethan <zhennankee@gmail.com>
2021-12-08 15:39:08 +0800

    Update store management

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,4 +4,8 @@ namespace App\Models;
 
 class Store extends BaseModel
 {
+    protected $casts = [
+        'region' => 'array',
+        'business_hours' => 'array',
+    ];
 }
--- database/migrations/2021_11_21_182854_create_stores_table.php
+++ database/migrations/2021_11_21_182854_create_stores_table.php
@@ -17,14 +17,13 @@ class CreateStoresTable extends Migration
             $table->id();
             $table->bigInteger('merchant_id');
             $table->string('name')->comment('店铺名称');
-            $table->string('photo')->comment('图片');
+            $table->string('photo')->nullable()->comment('图片');
             $table->string('contact', 20)->nullable()->comment('联系人');
             $table->string('phone', 11)->comment('手机号');
             $table->json('region')->nullable()->comment('地区');
             $table->string('address', 100)->nullable()->comment('详细地址');
+            $table->json('business_hours');
             $table->tinyInteger('status')->default(0)->comment('经营状态 0: 未启用, 1: 启用');
-            $table->timestamp('start_time');
-            $table->timestamp('end_time');
             $table->timestamps();
         });
     }
